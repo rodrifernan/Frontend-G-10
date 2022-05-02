6d35f885d4fb859e043a0ec60cde82459e1839f7
M0n00s <programacion.buffalo@gmail.com>
2022-05-02 08:52:07 -0400

    update/estilos

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,21 @@ import { Route } from "react-router-dom";
 import "./App.css";
 import Form from "./Components/Form/Form";
 import Registro from "./Components/Home/Registro";
-import Home from "./Components/Home/Home";
 import HomeAcrticles from "./Components/Home/Home-article";
 import { CreateProducts } from "./Components/createProduct/CreateProducts";
 import Header from "./Components/Headers/Header";
+import { Footer } from "./Components/footer/Footer";
 
 function App() {
 	return (
 		<>
 			<Route path="/" component={Header}></Route>
 			<Route exact path="/" component={HomeAcrticles}></Route>
-			<Route exact path="/Shop" component={Home}></Route>
+			{/* <Route exact path="/Shop" component={Home}></Route> */}
 			<Route path="/create" component={CreateProducts} />
 			<Route path="/register" component={Registro} />
 			<Route path="/registro" component={Form} />
+			<Route path="/" component={Footer}></Route>
 		</>
 	);
 }
