79c66ec1bfc51056ff70f5d1ef0f4ac1b5adb767
M0n00s <programacion.buffalo@gmail.com>
2022-05-11 17:04:21 -0400

    update/productform

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,51 +1,59 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import productsSlice from './reducer/products';
-import userSlice from './reducer/userPost';
-import categoriesSlice from './reducer/getCategorie';
-import carritoReducer from './reducer/carrito';
-import login from './reducer/login';
-import userPost from './reducer/userPost';
-import perfilSlice from './reducer/perfil';
-import wishSlice from './reducer/getWishilist';
-import allUserSlice from './reducer/getAllUsers';
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import productsSlice from "./reducer/products";
+import userSlice from "./reducer/userPost";
+import categoriesSlice from "./reducer/getCategorie";
+import carritoReducer from "./reducer/carrito";
+import login from "./reducer/login";
+import userPost from "./reducer/userPost";
+import perfilSlice from "./reducer/perfil";
+import wishSlice from "./reducer/getWishilist";
+import allUserSlice from "./reducer/getAllUsers";
+import genresSlice from "./reducer/getGenre";
 
-import checkoutMP from './reducer/checkoutMP';
+import checkoutMP from "./reducer/checkoutMP";
 //import paymentPM  from './reducer/getResponseMP'
-import paymentOrderPM  from './reducer/getResponseMP'
+import paymentOrderPM from "./reducer/getResponseMP";
 
-
-const persistConfig = { key: 'root', version: 1, storage };
+const persistConfig = { key: "root", version: 1, storage };
 const persistedReducer = persistReducer(persistConfig, carritoReducer);
 export const store = configureStore({
-  reducer: {
-    products: productsSlice,
-    user: userSlice,
-    categories: categoriesSlice,
-    carrito: persistedReducer,
-    login,
-    userPost,
-    perfil: perfilSlice,
-    wish: wishSlice,
-    AllUsers: allUserSlice,
-    //paymentId : paymentPM,
-    paymentOrderEG : paymentOrderPM,
-    checkout  : checkoutMP,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+	reducer: {
+		genres: genresSlice,
+		products: productsSlice,
+		user: userSlice,
+		categories: categoriesSlice,
+		carrito: persistedReducer,
+		login,
+		userPost,
+		perfil: perfilSlice,
+		wish: wishSlice,
+		AllUsers: allUserSlice,
+		//paymentId : paymentPM,
+		paymentOrderEG: paymentOrderPM,
+		checkout: checkoutMP,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					FLUSH,
+					REHYDRATE,
+					PAUSE,
+					PERSIST,
+					PURGE,
+					REGISTER,
+				],
+			},
+		}),
 });
