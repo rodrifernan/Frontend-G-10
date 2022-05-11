7d3f735efed1248be810a7e1a61e201629c7df59
M0n00s <programacion.buffalo@gmail.com>
2022-05-11 17:09:54 -0400

    update/ formProducts

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import {
-<<<<<<< HEAD
 	persistStore,
 	persistReducer,
 	FLUSH,
@@ -9,16 +8,6 @@ import {
 	PERSIST,
 	PURGE,
 	REGISTER,
-=======
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
->>>>>>> master
 } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import productsSlice from "./reducer/products";
@@ -30,10 +19,7 @@ import userPost from "./reducer/userPost";
 import perfilSlice from "./reducer/perfil";
 import wishSlice from "./reducer/getWishilist";
 import allUserSlice from "./reducer/getAllUsers";
-<<<<<<< HEAD
 import genresSlice from "./reducer/getGenre";
-=======
->>>>>>> master
 
 import checkoutMP from "./reducer/checkoutMP";
 //import paymentPM  from './reducer/getResponseMP'
@@ -42,7 +28,6 @@ import paymentOrderPM from "./reducer/getResponseMP";
 const persistConfig = { key: "root", version: 1, storage };
 const persistedReducer = persistReducer(persistConfig, carritoReducer);
 export const store = configureStore({
-<<<<<<< HEAD
 	reducer: {
 		genres: genresSlice,
 		products: productsSlice,
@@ -71,26 +56,4 @@ export const store = configureStore({
 				],
 			},
 		}),
-=======
-  reducer: {
-    products: productsSlice,
-    user: userSlice,
-    categories: categoriesSlice,
-    carrito: persistedReducer,
-    login,
-    userPost,
-    perfil: perfilSlice,
-    wish: wishSlice,
-    getAllUsers: allUserSlice,
-    //paymentId : paymentPM,
-    paymentOrderEG: paymentOrderPM,
-    checkout: checkoutMP,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
->>>>>>> master
 });
