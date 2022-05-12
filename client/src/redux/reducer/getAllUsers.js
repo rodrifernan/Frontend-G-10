076c01c150aaf6917fd5dba8a324ffe23c96b215
M0n00s <programacion.buffalo@gmail.com>
2022-05-11 22:03:06 -0400

    validate/Finish/Form

--- client/src/redux/reducer/getAllUsers.js
+++ client/src/redux/reducer/getAllUsers.js
@@ -2,36 +2,38 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getAllUsers = createAsyncThunk("users/getAllUsers", async () => {
-  const response = await axios
-    .get("/api/user/all", {
-      headers: {
-        "auth-token": JSON.parse(localStorage.getItem("userCredentials")).token,
-      },
-    })
+	const response = await axios
+		.get("/api/user/all", {
+			headers: {
+				"auth-token": JSON.parse(
+					localStorage.getItem("userCredentials")
+				).token,
+			},
+		})
 
-    .catch((err) => {
-      console.log(err);
-    });
-  return response.data;
+		.catch((err) => {
+			console.log(err);
+		});
+	return response.data;
 });
 
 const initialState = {
-  getAllUsers: [],
+	getAllUsers: [],
 };
 
 const allUserSlice = createSlice({
-  name: "getAllUsers",
-  initialState,
-  reducers: {},
-  extraReducers: {
-    [getAllUsers.pending]: () => {
-      console.log("Trayendo usuarios");
-    },
-    [getAllUsers.fulfilled]: (state, action) => {
-      return { ...state, getAllUsers: action.payload };
-    },
-  },
+	name: "getAllUsers",
+	initialState,
+	reducers: {},
+	extraReducers: {
+		[getAllUsers.pending]: () => {
+			console.log("Trayendo usuarios");
+		},
+		[getAllUsers.fulfilled]: (state, action) => {
+			return { ...state, getAllUsers: action.payload };
+		},
+	},
 });
 
 export default allUserSlice.reducer;
-export const allUserRegisters = (state) => state.getAllUsers.getAllUsers;
+export const allUserRegisters = (state) => state.AllUsers.getAllUsers;
