f82e1f46e7c8e875471d9f55425d8e96d3350ab8
Johannes Gómez <87533958+JohannesGomez@users.noreply.github.com>
2022-05-15 12:33:01 -0400

    Post-Order/Factura

--- client/src/Components/ResponseMP/responseMP.jsx
+++ client/src/Components/ResponseMP/responseMP.jsx
@@ -19,62 +19,60 @@ const ResponseMP = () => {
             
           let paymentIdOrdenEl = useSelector(state => state.paymentOrderEG.paymentId);
           let getOrderMPEl = useSelector(state => state.paymentOrderEG.paymentOrder);
-          let nroOrdenCompraEl = useSelector(state => state.paymentOrderEG.nroOrdenCompra);
+          let getOrderAllEl = useSelector(state => state.paymentOrderEG.OrdenCompra);
           
-
           useEffect(() => {
             dispatch(getPaymentIdMP(payment_id));
             dispatch(getOrderMP(getOrderMPEl));        
             dispatch(postOrderMP());        
             setState(getOrderMPEl)
-
-            //dispatch(postOrderMP(getOrderMPEl));        
           }, []);
-
-         // console.log('dispatch(Orden Compra      ->',getOrderMPEl)
-          //console.log('dispatch(NRO. Orden Compra ->',nroOrdenCompraEl.invoiceNumber)
-         // console.log('dispatch(getOrderMP        ->',Object.keys(getOrderMPEl).length)
-          let subTotal = 0
-          let totalPagar = 0
-         //console.log('estoy  Componente ResponseMP payment_id', payment_id);
-
-        // Determinar tipo de transaccion mercado pago
-        // tipoTransMP(paymentIdOrdenEl?.card?.cardholder?.name)
-       if(Object.keys(getOrderMPEl).length>0)genPDF()        
+         // console.log(getOrderMPEl)
+         // console.log(getOrderAllEl?.orderBd?.orderNumber)
+          let subTotal      = 0
+          let totalPagar    = 0
+          let descuentoItem = 0
+          //if(Object.keys(getOrderMPEl).length>0)genPDF()        
     
     return ( //getOrderMPEl.length
-        Object.keys(getOrderMPEl).length>0?
+        Object.keys(getOrderMPEl).length>0 ?
     
-        <div id="content" className="page-content container" style = {{background:'white', height:'800px'}}>
-                    <div className="page-header text-blue-d2">
+        // <div id="content" className="page-content container" style = {{background:'white', height:'800px'}}>
 
-                        <div className="page-tools">
-                            <div className="action-buttons">
-                                <a className="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
-                                    <i className="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
-                                    Imprimir
-                                </a>
-                                <a className="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
-                                    <i className="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
-                                    Exportar
-                                </a>
-                            </div>
+
+    <div id="content" class="page-content container" style = {{background:'white', height:'800px'}}>
+                <div class="page-header text-blue-d2">
+                    <h1 class="page-title text-secondary-d1">
+                    ShopBag
+                    </h1>
+
+                    {/* <div class="page-tools">
+                        <div class="action-buttons">
+                            <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
+                                <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
+                                Print
+                            </a>
+                            <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
+                                <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
+                                Export
+                            </a>
                         </div>
-                    </div>
+                    </div> */}
+                </div>
 
-            <div className="container px-0">
-                <div className="row mt-4">
-                    <div className="col-12 col-lg-12">
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="text-center text-150">
-                                    <i className="fa fa-book fa-2x text-success-m2 mr-1"></i>
-                                    <span className="text-default-d3">Orden Compra Nro. </span>
-                                    <span className="text-default-d3">{nroOrdenCompraEl?.invoiceNumber}</span>
-                                    
+                <div class="container px-0">
+                    <div class="row mt-4">
+                       <div class="col-12 col-lg-12">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="text-center text-150">
+                                            <i class="fa fa-book fa-2x text-success-m2 mr-1"></i>
+                                            <span class="text-default-d3">Orden Compra # {getOrderAllEl?.orderBd?.orderNumber}</span>
+                                        </div>
+
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
+
 
                         <hr className="row brc-default-l1 mx-n1 mb-4" />
 
@@ -82,42 +80,53 @@ const ResponseMP = () => {
                             <div className="col-sm-6">
                                 <div>
                                     <span className="text-sm text-grey-m2 align-middle">Para:</span>
-                                    <span className="text-600 text-110 text-blue align-middle"> {getOrderMPEl.user.firstName} {getOrderMPEl.user.lastName}</span> 
+                                    <span className="text-600 text-110 text-blue align-middle"> {getOrderMPEl?.user?.firstName} {getOrderMPEl?.user?.lastName}</span> 
                                 </div>
                                 <div>
                                     <span className="text-sm text-grey-m2 align-middle">Dirección:</span>
-                                    <span className="text-600 text-110 text-blue align-middle">{getOrderMPEl.user.address}</span> 
+                                    <span className="text-600 text-110 text-blue align-middle">{getOrderMPEl?.user?.address}</span> 
                                 </div>
                                 <div>
                                     <span className="text-sm text-grey-m2 align-middle">Teléfono:</span>
-                                    <span className="text-600 text-110 text-blue align-middle">{getOrderMPEl.user.phone}</span> 
+                                    <span className="text-600 text-110 text-blue align-middle">{getOrderMPEl?.user?.phone}</span> 
                                 </div>
                                 <div>
                                     <span className="text-sm text-grey-m2 align-middle">Email: </span>
-                                    <span className="text-600 text-110 text-blue align-middle">{getOrderMPEl.user.email}</span> 
+                                    <span className="text-600 text-110 text-blue align-middle">{getOrderMPEl?.user?.email}</span> 
                                 </div>
-
                             </div>
 
-                        </div>
+                            <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
+                                <hr class="d-sm-none" />
+                                <div class="text-grey-m2">
+                                    <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125"></div>
+                                    <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Orden #:</span>{getOrderAllEl?.orderBd?.orderNumber}</div>
+                                    <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Fecha:</span>{getOrderAllEl?.orderBd?.createdAt}</div>
+                                    <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> 
+                                    <span class="badge badge-warning badge-pill px-25">{getOrderAllEl?.orderBd?.status}</span></div>
+                                </div>
+                             </div>
 
-                        <div class="mt-4">           
-                            <div className="row text-600 text-white bgc-default-tp1 py-25"
-                            >
+
+                           </div>
+
+                           <div className="row text-600 text-white bgc-default-tp1 py-25">
                                 <div className="d-none d-sm-block col-1">#</div>
                                 <div className="col-9 col-sm-5">Descripción</div>
                                 <div className="d-none d-sm-block col-4 col-sm-2">Cant.</div>
                                 <div className="d-none d-sm-block col-sm-2">Unit.Precio</div>
                                 <div className="col-2">Monto</div>
-
                             </div>
+
+                        <div class="mt-4">           
+                            
 
                             <div className="text-95 text-secondary-d3">
                                     {
-                                    
-                                    getOrderMPEl.shoppingCart.map((elem ) => {
-                                        subTotal   = subTotal   + elem.total
-                                        totalPagar = totalPagar + elem.subTotal
+                                    getOrderMPEl?.shoppingCart?.map((elem ) => {
+                                        subTotal      = subTotal      + elem.subTotal
+                                        totalPagar    = totalPagar    + elem.totalPage
+                                        descuentoItem = descuentoItem + elem.discountItem
                                     return(
                                         
                                             <div key={elem.title} className="row mb-2 mb-sm-0 py-25">
@@ -125,25 +134,36 @@ const ResponseMP = () => {
                                                 <div className="col-9 col-sm-5">{elem.title}</div> 
                                                 <div className="d-none d-sm-block col-2">{elem.quantity}</div>
                                                 <div className="d-none d-sm-block col-2 text-95">{elem.price}</div>
-                                                <div className="col-2 text-secondary-d2">{elem.quantity*elem.price}</div>
+                                                <div className="col-2 text-secondary-d2">{elem.subTotal}</div>
                                             </div>
                                     )})
-                            } 
+                                    } 
                            </div>
                         </div>
                             
                         {/*Totales Compra*/}
                 <div class="row mt-3">
                         <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
+
+                            
                             <div class="row my-2">
-                                <div class="col-7 text-right">Sub-Total</div>
+                                <div class="col-7 text-right">Sub-Total $</div>
                                 <div class="col-5">
                                     <span class="text-120 text-secondary-d1">{subTotal}</span>
                                 </div>
                             </div>
 
+                            <div class="row my-2">
+                                <div class="col-7 text-right">
+                                    Descuento $
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-110 text-secondary-d1">{descuentoItem}</span>
+                                </div>
+                            </div>
+
                             <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                <div class="col-7 text-right">Total a Pagar</div>
+                                <div class="col-7 text-right">Total a Pagar $</div>
                                 <div class="col-5">
                                     <span class="text-150 text-success-d3 opacity-2">{totalPagar}</span>
                                 </div>
@@ -160,60 +180,24 @@ const ResponseMP = () => {
              </div>
         </div>
         
-        :""
+        :<></>
         
         
                   );
 };
 export default ResponseMP
 
-const genPDF = () =>
-  {
-//     <div id="elementH"></div>
-
-//     var doc = new jsPDF();
-//     var elementHTML = document.getElementById("content") //$('#content').html();
-//     //
-//     console.log('Archivo HTML -->',elementHTML.innerHTML)
-
-//      var specialElementHandlers = {
-//          '#elementH': function (element, renderer) {
-//              return true;
-//          }
-//      };
-//     doc.html(elementHTML.innerHTML, {
-//         'x': 15,
-//         'y': 15,
-//         'width': 170,
-//         'elementHandlers': specialElementHandlers
-//     });
-//     // doc.html(DATA.innerHTML, {
-//     //     'x': 15,
-//     //     'y': 15,
-//     //     'width': 200,
-//     //     'elementHandlers': handleElement
-//     //   });
-//      // // Save the PDF
-//    doc.save('sample1.pdf');    
-
-
-const jsPDF1 = new jsPDF();
-
-      var doc = new jsPDF('l', 'mm', [1200, 1810]);
-      var pdfjs = document.getElementById('content');
-
-      doc.html(pdfjs, {
-          callback: function(doc) {
-              doc.save("OrdenCompra.pdf");
-          },
-          x: 10,
-          y: 10
-      });
-
-      doc.output('dataurlnewwindow');
-
-
-  }
+const genPDF = async () => {
+        const jsPDF1 = new jsPDF();
+        var doc = new jsPDF('l', 'mm', [1200, 1810]);
+        var pdfjs =  document.getElementById('content');
+        doc.html(pdfjs, {
+            callback: function(doc) {doc.save("OrdenCompra.pdf")},
+            x: 10,
+            y: 10
+        });
+        doc.output('dataurlnewwindow');
+}
 
 // const tipoTransMP = (payload) => { 
 //     let tipoTransaccion = ''     
--- client/src/redux/reducer/getResponseMP.js
+++ client/src/redux/reducer/getResponseMP.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const userToken = JSON.parse(localStorage.getItem('userCredentials')).token
+
 export const getPaymentIdMP = createAsyncThunk(
 	"getResponseMp/getPaymentIdMP",
 	async (payload) => {
@@ -10,7 +12,6 @@ export const getPaymentIdMP = createAsyncThunk(
 			.catch((err) => {
 			console.log(err);
 		});
-		console.log('estoy accion getPaymentIdM->', payload)
 		return response.data;
 	}
 );
@@ -18,40 +19,33 @@ export const getPaymentIdMP = createAsyncThunk(
 export const getOrderMP = createAsyncThunk(
 	"getResponseMp/getOrderMP",
 	async (payload) => {
-		
-		const userToken = JSON.parse(localStorage.getItem('userCredentials')).token
 		const response = await axios
-			.get(`http://localhost:3001/api/orders/checkout`,{ headers: { "auth-token": userToken },})
+			.get(`/api/orders/checkout`,
+			{ headers: { "auth-token": userToken },})
 			.catch((err) => {
 			console.log(err);
 		});
-		console.log('estoy accion getOrderMP->', payload)
 		return response.data;
-
 	}
 );
 
 export const postOrderMP = createAsyncThunk(
 	"getResponseMp/postOrderMP",
 	async () => {
-		
 		const response = await axios
-			.post(`http://localhost:3001/api/invoice`,{},{
-                headers:  { "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5Y2FjYjNjLTRlZjMtNGQ3Mi1iYmYzLWQ2NjE4ZTQ1YTQ1ZCIsImlhdCI6MTY1MjMxMjYwNSwiZXhwIjoxNjUyMzk5MDA1fQ.BxDAs2VCRMoOARBQvO9qbzQq22m_6qY7uQDRcwHls98" },
-            })
+			.post(`/api/invoice`,{},
+			{ headers: { "auth-token": userToken },})
 			.catch((err) => {
 			console.log(err);
 		});
-		console.log('estoy accion postOrderMP->',response.data)
 		return response.data;
-
 	}
 );
 
 const initialState = {
 	paymentId: [],
 	paymentOrder : [],
-	nroOdenCompra: {},
+	OdenCompra: [],
 };
 
 const paymentIdSlice = createSlice({
@@ -79,8 +73,8 @@ const paymentIdSlice = createSlice({
 			//console.log("Trayendo Datos MercadoPago Compra");
 		},
 		[postOrderMP.fulfilled]: (state, action) => {
-            //console.log("Trayendo Datos MercadoPago Compra", action.payload)
-			return {...state, nroOrdenCompra: action.payload };
+            console.log("Trayendo Nro Orden MercadoPago Compra", action.payload)
+			return {...state, OrdenCompra: action.payload };
 		},		
 	},
 });
