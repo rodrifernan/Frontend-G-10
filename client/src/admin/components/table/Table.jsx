74eb4cc9b419d2f95dafedbc0bc27b87e8ecd4a7
lalechelml <ramos.joshelin20@gmail.com>
2022-05-16 15:42:21 -0500

    admin notifications

--- client/src/admin/components/table/Table.jsx
+++ client/src/admin/components/table/Table.jsx
@@ -1,92 +1,68 @@
-import "./table.scss";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import './table.scss';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useState, useEffect } from 'react';
+import { sendNotification } from '../../../utils/notifications';
 
-const List = () => {
-  const rows = [
-    {
-      id: 1143155,
-      product: "Acer Nitro 5",
-      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 785,
-      // method: "Cash on Delivery",
-      status: "Aprobado",
-    },
-    {
-      id: 2235235,
-      product: "Playstation 5",
-      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Michael Doe",
-      date: "1 March",
-      amount: 900,
-      // method: "Online Payment",
-      status: "Pending",
-    },
-    {
-      id: 2342353,
-      product: "Redragon S101",
-      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "John Smith",
-      date: "1 March",
-      amount: 35,
-      // method: "Cash on Delivery",
-      status: "Pending",
-    },
-    {
-      id: 2357741,
-      product: "Razer Blade 15",
-      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Jane Smith",
-      date: "1 March",
-      amount: 920,
-      // method: "Online",
-      status: "Aprobado",
-    },
-    {
-      id: 2342355,
-      product: "ASUS ROG Strix",
-      img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
-      customer: "Harold Carol",
-      date: "1 March",
-      amount: 2000,
-      // method: "Online",
-      status: "Pending",
-    },
-  ];
+const List = ({ socket }) => {
+  const [rows, setRows] = useState([]);
+
+  const filterRows = orders => {
+    setRows(
+      [...orders].map(order => ({
+        id: order.orderNumber,
+        product: order.product.name,
+        img: order.product.image[0],
+        customer: order.user.firstName + ' ' + order.user.lastName,
+        date: order.orderDate,
+        amount: '$' + order.total,
+        status: order.status,
+      }))
+    );
+  };
+
+  useEffect(() => {
+    sendNotification('lastOrders').then(({ data }) =>
+      filterRows(data.response)
+    );
+  }, []);
+
+  socket.on('lastOrders', orders => {
+    filterRows(orders);
+  });
+
   return (
-    <TableContainer component={Paper} className="table">
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer component={Paper} className='table'>
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
           <TableRow>
-            <TableCell className="tableCell">Tracking ID</TableCell>
-            <TableCell className="tableCell">Producto</TableCell>
-            <TableCell className="tableCell">Usuario</TableCell>
-            <TableCell className="tableCell">Fecha</TableCell>
-            <TableCell className="tableCell">Monto</TableCell>
+            <TableCell className='tableCell'>Tracking ID</TableCell>
+            <TableCell className='tableCell'>Producto</TableCell>
+            <TableCell className='tableCell'>Usuario</TableCell>
+            <TableCell className='tableCell'>Fecha</TableCell>
+            <TableCell className='tableCell'>Monto</TableCell>
             {/* <TableCell className="tableCell">Pago </TableCell> */}
             {/* <TableCell className="tableCell">Estado</TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {rows.map(row => (
             <TableRow key={row.id}>
-              <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell">
-                <div className="cellWrapper">
-                  <img src={row.img} alt="" className="image" />
+              <TableCell className='tableCell'>{row.id}</TableCell>
+              <TableCell className='tableCell'>
+                <div className='cellWrapper'>
+                  <img src={row.img} alt='' className='image' />
                   {row.product}
                 </div>
               </TableCell>
-              <TableCell className="tableCell">{row.customer}</TableCell>
-              <TableCell className="tableCell">{row.date}</TableCell>
-              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className='tableCell'>{row.customer}</TableCell>
+              <TableCell className='tableCell'>{row.date}</TableCell>
+              <TableCell className='tableCell'>{row.amount}</TableCell>
               {/* <TableCell className="tableCell">{row.method}</TableCell> */}
               {/* <TableCell className="tableCell">
                 <span className={`status ${row.status}`}>{row.status}</span>
