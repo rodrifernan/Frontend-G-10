9389ed8009d499daf54edd5f533e227fec022d34
Mario Meza <78285858+Leojr2021@users.noreply.github.com>
2022-05-17 17:39:13 -0500

    se configuro los graficos

--- client/src/admin/components/Recharts/PieChar/PieChar.jsx
+++ client/src/admin/components/Recharts/PieChar/PieChar.jsx
@@ -1,70 +1,95 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from "react";
+import './PieChar.scss'
+import { useSelector } from "react-redux";
+import {
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
 
-
-
-
-
+import { getAllProducts } from "../../../../redux/reducer/products";
 
 export const PieChar = () => {
+  let data1 = useSelector(getAllProducts);
+  console.log(data1);
 
+  let bellezaArray = data1.filter((e) => e.category === "Belleza");
+  // console.log(bellezaArray.length);
+  let deporteArray = data1.filter((e) => e.category === "Deporte");
+  // console.log(deporteArray.length);
+  let MascotaArray = data1.filter((e) => e.category === "Mascosta");
+  // console.log(MascotaArray.length);
+  let electronicaArray = data1.filter((e) => e.category === "Electronica");
+  // console.log(electronicaArray.length);
 
   const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 },
+    { name: "Belleza", value: bellezaArray.length },
+    { name: "Deporte", value: deporteArray.length },
+    { name: "Mascota", value: MascotaArray.length },
+    { name: "Electronica", value: electronicaArray.length },
   ];
-  
-  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-  
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
   const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+    value,
+  }) => {
+    const radius = 25 + innerRadius + (outerRadius - innerRadius);
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
     const y = cy + radius * Math.sin(-midAngle * RADIAN);
-  
+
     return (
-      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-        {`${(percent * 100).toFixed(0)}%`}
+      <text
+        x={x}
+        y={y}
+        fill="black"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {/* {`${(percent * 100).toFixed(0)}%`} */}
+        {data[index].name} ({value})
       </text>
     );
   };
 
-
-
-
-
-
   return (
-    <ResponsiveContainer >
-    <PieChart width={400} height={400}>
-      <Pie
-        data={data}
-        cx="50%"
-        cy="50%"
-        labelLine={false}
-        label={renderCustomizedLabel}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChart>
-  </ResponsiveContainer>
-  )
-}
-
-
-
-
-
-
-
-
-
-
-
+    <>
+      <div className="container_piechart">
+      <h4>Catidad de productos vs categoria</h4>
+      <ResponsiveContainer>
+        <PieChart width={400} height={400}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            // labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={80}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+      </div>
+    </>
+  );
+};
--- client/src/admin/components/Recharts/RadarChar/RadarChar.jsx
+++ client/src/admin/components/Recharts/RadarChar/RadarChar.jsx
@@ -1,66 +1,105 @@
-import React, { PureComponent } from 'react';
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+import React, { PureComponent } from "react";
+import { useSelector } from "react-redux";
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  ResponsiveContainer,
+} from "recharts";
 
-
-
+import { getAllProducts } from "../../../../redux/reducer/products";
 
 export const RadarChar = () => {
+  let data1 = useSelector(getAllProducts);
 
-
+  let bellezaArray = data1
+    .filter((e) => e.category === "Belleza")
+    .map((e) => e.stock);
+  let sumStockBelleza = bellezaArray.reduce(
+    (pvalue, current) => pvalue + current,
+    0
+  );
+  // console.log(bellezaArray);
+  // console.log(sumStockBelleza);
+  let deporteArray = data1
+    .filter((e) => e.category === "Deporte")
+    .map((e) => e.stock);
+  let sumStockDeporte = deporteArray.reduce(
+    (pvalue, current) => pvalue + current,
+    0
+  );
+  // console.log(deporteArray.length);
+  let MascotaArray = data1
+    .filter((e) => e.category === "Mascosta")
+    .map((e) => e.stock);
+  let sumStockMascosta = MascotaArray.reduce(
+    (pvalue, current) => pvalue + current,
+    0
+  );
+  // console.log(MascotaArray.length);
+  let electronicaArray = data1
+    .filter((e) => e.category === "Electronica")
+    .map((e) => e.stock);
+  let sumStockElectronica = electronicaArray.reduce(
+    (pvalue, current) => pvalue + current,
+    0
+  );
+  // console.log(electronicaArray.length);
+  const arrayCategorias = [
+    sumStockBelleza,
+    sumStockDeporte,
+    sumStockMascosta,
+    sumStockElectronica,
+  ];
+  // console.log(arrayCategorias);
+  let mayorStockCategoria = Math.max(arrayCategorias);
 
   const data = [
     {
-      subject: 'Belleza',
-      A: 120,
-      B: 130,
-      fullMark: 150,
+      subject: "Belleza",
+      A: sumStockBelleza,
+
+      fullMark: mayorStockCategoria,
     },
     {
-      subject: 'Deporte',
-      A: 98,
-      B: 130,
-      fullMark: 150,
+      subject: "Deporte",
+      A: sumStockDeporte,
+
+      fullMark: mayorStockCategoria,
     },
     {
-      subject: 'Electronica',
-      A: 86,
-      B: 130,
-      fullMark: 150,
+      subject: "Electronica",
+      A: sumStockMascosta,
+
+      fullMark: mayorStockCategoria,
     },
     {
-      subject: 'Mascota',
-      A: 99,
-      B: 130,
-      fullMark: 150,
+      subject: "Mascota",
+      A: sumStockElectronica,
+
+      fullMark: mayorStockCategoria,
     },
-    // {
-    //   subject: 'Physics',
-    //   A: 85,
-    //   B: 90,
-    //   fullMark: 150,
-    // },
-    // {
-    //   subject: 'History',
-    //   A: 65,
-    //   B: 85,
-    //   fullMark: 150,
-    // },
   ];
 
-
-
-
   return (
-    <ResponsiveContainer width="100%" height="100%">
+    <>
+      <h4>Stock de productos vs categoria</h4>
+      <ResponsiveContainer width="100%" height="100%">
         <RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
           <PolarGrid />
           <PolarAngleAxis dataKey="subject" />
           <PolarRadiusAxis />
-          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.5} />
+          <Radar
+            name="Mike"
+            dataKey="A"
+            stroke="#8884d8"
+            fill="#8884d8"
+            fillOpacity={0.5}
+          />
         </RadarChart>
       </ResponsiveContainer>
-  )
-}
-
-
-
+    </>
+  );
+};
