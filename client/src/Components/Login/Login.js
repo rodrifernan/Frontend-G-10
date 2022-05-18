aad7629a322fdb1312b4b52ae2e9b5ef4dd1d427
rodrifernan <rodrifernandez8@gmail.com>
2022-05-18 16:38:15 -0300

    Arreglo Login

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -183,7 +183,7 @@ export const Login = ({ redirect = null, page = false, loginClass = "" }) => {
         </button>
         <LoginFromGoogle page />
         <Link to="/process-password">
-          <p onClick={hideModal()} className="mt-3">¿Ha olvidado su contraseña? ¡Haga click aquí!</p>
+          <p onClick={hideModal} className="mt-3">¿Ha olvidado su contraseña? ¡Haga click aquí!</p>
         </Link>
       </div>
     </div>
