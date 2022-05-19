29c56dd47f5ff0a5fc492778dec821137a96a3fe
Mario Meza <78285858+Leojr2021@users.noreply.github.com>
2022-05-18 21:06:56 -0500

    searchbar fix

--- client/src/Components/Headers/SearchBar.jsx
+++ client/src/Components/Headers/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { getByName } from "../../redux/reducer/products";
 import { useDispatch } from "react-redux";
 import "./SearchBar.css";
+import { BsSearch } from 'react-icons/bs';
 
 const SearchBar = () => {
   const dispatch = useDispatch();
@@ -17,21 +18,38 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="search">
-      <div className="search-content justify-content-start d-flex">
-        <div className="input-group inputSearch rounded ">
-          <input
-            type="text"
-            placeholder="Buscar"
-            onChange={handleOnChange}
-            required
-          />
+    // <div className="search">
+    //   <div className="search-content justify-content-start d-flex">
+    //     <div className="input-group inputSearch rounded ">
+    //       <input
+    //         type="text"
+    //         placeholder="Buscar"
+    //         onChange={handleOnChange}
+    //         required
+    //       />
 
-          <i
-            class="fas fa-search icon searchButton"
-            onClick={handleOnSubmit}
-          ></i>
-        </div>
+    //       <i
+    //         class="fas fa-search icon searchButton"
+    //         onClick={handleOnSubmit}
+    //       ></i>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="searchbar_container">
+      <div className="searchbar">
+        <form onSubmit={handleOnSubmit}>
+          <input
+            value={input}
+            onChange={handleOnChange}
+            placeholder="Buscar"
+            type="text"
+          ></input>
+
+          <button name="name" type="submit">
+            <BsSearch/>
+          </button>
+        </form>
       </div>
     </div>
   );
